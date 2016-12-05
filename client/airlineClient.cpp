9e14cf95d5bdefdf337d0ad869c3810fb16f383a
persephonelu <fanglu2004@gmail.com>
2016-12-04 21:55:21 -0800

    update airline client

--- client/airlineClient.cpp
+++ client/airlineClient.cpp
@@ -8,13 +8,19 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <limits.h>
+#include <iostream>
 
 #define MAXBUFFSIZE 10240
 
 char buf[MAXBUFFSIZE];
 
+int logon(int connfd);
+int signup(int connfd);
+int signin(int connfd);
 int startService(int connfd);
 int addFlight(int connfd);
+int listFlight(int connfd);
+int delFlight(int connfd);
 
 int main(int argc, char** argv)
 {
@@ -52,11 +58,184 @@ int main(int argc, char** argv)
 		exit(1);
 	}
 
-	startService(clientfd);
-
+    if (logon(clientfd) == 0)
+    {
+        startService(clientfd);
+    }
+	
 	close(clientfd);
 
 	return 0;
+}
+
+int logon(int connfd)
+{
+    while (1)
+    {
+        bzero(buf, MAXBUFFSIZE);
+        if (recv(connfd, buf, MAXBUFFSIZE, 0) < 0)
+        {
+            perror("recv Data from server");
+            return -1;
+        }
+
+        printf("%s", buf);
+        bzero(buf, MAXBUFFSIZE);
+
+        int select = 0;
+        scanf("%d", &select);
+        getchar();
+        sprintf(buf, "%d", select);
+        if (send(connfd, buf, strlen(buf), 0) < 0)
+        {
+            perror("Sending filename to server failed\n");  //fail to send the filename
+            return -1;
+        }
+
+        bzero(buf, MAXBUFFSIZE);
+        if (recv(connfd, buf, 1, 0) < 0)
+        {
+            perror("recv Data from server");
+            return -1;
+        }
+
+        int ret = atoi(buf);
+
+        if (ret == 0)
+        {
+            if (select == 1)
+            {
+                if (signup(connfd) == 0)
+                {
+                    return 0;
+                }
+            }
+            else if (select == 2)
+            {
+                if (signin(connfd) == 0)
+                {
+                    return 0;
+                }
+            }
+            else if (select == 3)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int signup(int connfd)
+{
+    //username
+    bzero(buf, MAXBUFFSIZE);
+    if(recv(connfd, buf, MAXBUFFSIZE, 0) < 0)
+    {
+        perror("read data from client");
+        return -1;
+    }
+    printf("%s", buf);
+
+    bzero(buf, MAXBUFFSIZE);
+    scanf("%[^\n]", buf);
+    getchar();
+    if (send(connfd, buf, strlen(buf), 0) < 0)
+    {
+        perror("Send data to client");
+        return -1;
+    }
+
+    //password
+    bzero(buf, MAXBUFFSIZE);
+    if(recv(connfd, buf, MAXBUFFSIZE, 0) < 0)
+    {
+        perror("read data from client");
+        return -1;
+    }
+    printf("%s", buf);
+
+    bzero(buf, MAXBUFFSIZE);
+    scanf("%[^\n]", buf);
+    getchar();
+    if (send(connfd, buf, strlen(buf), 0) < 0)
+    {
+        perror("Send data to client");
+        return -1;
+    }
+
+    //result
+    bzero(buf, MAXBUFFSIZE);
+    if(recv(connfd, buf, 1, 0) < 0)
+    {
+        perror("read data from client");
+        return -1;
+    }
+    int ret = atoi(buf);
+    if (ret == 1)
+    {
+        printf("Sign up failed. Please retry.\n");
+        return -1;
+    }
+
+    printf("Sign up success.\n");
+    return 0;
+}
+
+int signin(int connfd)
+{
+    //username
+    bzero(buf, MAXBUFFSIZE);
+    if(recv(connfd, buf, MAXBUFFSIZE, 0) < 0)
+    {
+        perror("read data from client");
+        return -1;
+    }
+    printf("%s", buf);
+
+    bzero(buf, MAXBUFFSIZE);
+    scanf("%[^\n]", buf);
+    getchar();
+    if (send(connfd, buf, strlen(buf), 0) < 0)
+    {
+        perror("Send data to client");
+        return -1;
+    }
+
+    //password
+    bzero(buf, MAXBUFFSIZE);
+    if(recv(connfd, buf, MAXBUFFSIZE, 0) < 0)
+    {
+        perror("read data from client");
+        return -1;
+    }
+    printf("%s", buf);
+
+    bzero(buf, MAXBUFFSIZE);
+    scanf("%[^\n]", buf);
+    getchar();
+    if (send(connfd, buf, strlen(buf), 0) < 0)
+    {
+        perror("Send data to client");
+        return -1;
+    }
+
+    //result
+    bzero(buf, MAXBUFFSIZE);
+    if(recv(connfd, buf, 1, 0) < 0)
+    {
+        perror("read data from client");
+        return -1;
+    }
+    int ret = atoi(buf);
+    if (ret == 1)
+    {
+        printf("Sign in Failed. Please retry.\n");
+        return -1;
+    }
+
+    printf("Sign in success.\n");
+    return 0;
 }
 
 int startService(int connfd)
@@ -100,19 +279,53 @@ int startService(int connfd)
 		{
 			if (select == 1)
 			{
-				if (addFlight(connfd) < 0)
+				if (listFlight(connfd) < 0)
 				{
 					return -1;
 				}
 			}
 			else if (select == 2)
 			{
-				break;
+				if (addFlight(connfd) < 0)
+                {
+                    return -1;
+                }
 			}
+            else if (select == 3)
+            {
+                if (delFlight(connfd) < 0)
+                {
+                    return -1;
+                }
+            }
+            else if (select == 4)
+            {
+                break;
+            }
 		}
 	}
 
 	return 0;
+}
+
+int listFlight(int connfd)
+{
+    bzero(buf, MAXBUFFSIZE);
+
+    if (recv(connfd, buf, MAXBUFFSIZE, 0) < 0)
+    {
+        perror("recv Data from server");
+        return -1;
+    }
+    printf("%s", buf);
+
+    bzero(buf, MAXBUFFSIZE);
+    sprintf(buf, "0");
+    if (send(connfd, buf, 1, 0) < 0)
+    {
+        return -1;
+    }
+    return 0;
 }
 
 int addFlight(int connfd)
@@ -352,6 +565,92 @@ int addFlight(int connfd)
     if (send(connfd, buf, 1, 0) < 0)
     {
     	return -1;
+    }
+
+    return 0;
+}
+
+int delFlight(int connfd)
+{
+    bzero(buf, MAXBUFFSIZE);
+
+    int recvBytes = recv(connfd, buf, MAXBUFFSIZE, 0);
+    if (recvBytes < 0)
+    {
+        perror("recv Data from server");
+        return -1;
+    }
+
+    int ret = atoi(buf);
+    if (ret == 1)
+    {
+        printf("Not found flight recorders\n");
+        buf[0] = '1';
+        if (send(connfd, buf, 1, 0) < 0)
+        {
+            return -1;
+        }
+        return 0;
+    }
+
+    printf("%s", buf);
+    bzero(buf, MAXBUFFSIZE);
+
+    int select = 0;
+    scanf("%d", &select);
+    getchar();
+    sprintf(buf, "%d", select);
+
+    int sendBytes = send(connfd, buf, strlen(buf), 0);
+    if (sendBytes < 0)
+    {
+        perror("Sending filename to server failed\n");  //fail to send the filename
+        return -1;
+    }
+
+    bzero(buf, MAXBUFFSIZE);
+    recvBytes = recv(connfd, buf, MAXBUFFSIZE, 0);
+    if (recvBytes < 0)
+    {
+        perror("recv Data from server");
+        return -1;
+    }
+
+    ret = atoi(buf);
+    if (ret != 0)
+    {
+        printf("The input row number is invalid.\n");
+        buf[0] = '1';
+        if (send(connfd, buf, 1, 0) < 0)
+        {
+            return -1;
+        }
+        return 0;
+    }
+    printf("%s", buf);
+    bzero(buf, MAXBUFFSIZE);
+    scanf("%s", buf);
+    getchar();
+
+    if (send(connfd, buf, strlen(buf), 0) < 0)
+    {
+        perror("Sending filename to server failed\n");  //fail to send the filename
+        return -1;
+    }
+
+    bzero(buf, MAXBUFFSIZE);
+    recvBytes = recv(connfd, buf, MAXBUFFSIZE, 0);
+    if (recvBytes < 0)
+    {
+        perror("recv Data from server");
+        return -1;
+    }
+    printf("%s", buf);
+
+    buf[0] = '0';
+    if (send(connfd, buf, 1, 0) < 0)
+    {
+        return -1;
     }
 
     return 0;
